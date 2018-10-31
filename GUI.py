bb0d38891ec48c0494396a8b4e708c57ad91a1ed
Detmer Struiksma <detmer56@gmail.com>
2018-10-31 12:22:01 +0100

    Structuur verbeterd

--- GUI.py
+++ GUI.py
@@ -8,15 +8,21 @@ class App:
     def __init__(self, master):
         self.master = master
         master.title('Centrale')
+        self.notebook = ttk.Notebook(master)
+        self.tab1 = ttk.Frame(master)
+        self.tab2 = ttk.Frame(master)
+        self.tab3 = ttk.Frame(master)
+        self.tab4 = ttk.Frame(master)
+        self.tab5 = ttk.Frame(master)
+
+        self.graph1 = Canvas(self.tab1, width=675, height=475)
+        self.graph2 = Canvas(self.tab2, width=675, height=475)
+        self.graph3 = Canvas(self.tab3, width=675, height=475)
+        self.graph4 = Canvas(self.tab4, width=675, height=475)
+        self.graph5 = Canvas(self.tab5, width=675, height=475)
 
 
     def initialiseren(self):
-        self.notebook = ttk.Notebook(app.master)
-        self.tab1 = ttk.Frame(app.master)
-        self.tab2 = ttk.Frame(app.master)
-        self.tab3 = ttk.Frame(app.master)
-        self.tab4 = ttk.Frame(app.master)
-        self.tab5 = ttk.Frame(app.master)
         self.notebook.add(self.tab1, text='Arduino1')
         self.notebook.add(self.tab2, text='Arduino2')
         self.notebook.add(self.tab3, text='Arduino3')
@@ -24,21 +30,12 @@ class App:
         self.notebook.add(self.tab5, text='Arduino5')
         self.notebook.grid(column=0, row=0)
 
-        self.graph1 = Canvas(self.tab1, width=675, height=475)
-        self.graph1.create_rectangle([10, 10, 650, 450], fill='blue')  # placeholder voor grafiek
+        self.graph1.create_rectangle([10, 10, 650, 450], fill='blue')   #placeholder voor grafiek
         self.graph1.grid(column=1, row=0)
-
-        self.graph2 = Canvas(self.tab2, width=675, height=475)
-        self.graph2.create_rectangle([10, 10, 650, 450], fill='red')  # placeholder voor grafiek
+        self.graph2.create_rectangle([10, 10, 650, 450],fill='red')     #placeholder voor grafiek
         self.graph2.grid(column=2, row=0)
-
-        self.graph3 = Canvas(self.tab3, width=675, height=475)
         self.graph3.grid(column=3, row=0)
-
-        self.graph4 = Canvas(self.tab4, width=675, height=475)
         self.graph4.grid(column=4, row=0)
-
-        self.graph5 = Canvas(self.tab5, width=675, height=475)
         self.graph5.grid(column=5, row=0)
 
         self.uitrollen = Button(app.master, text="  Uitrollen   ", command=app.rolluik_uitrollen)
