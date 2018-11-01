7041c6da3298452420039911b57d21341cf93fe8
annemarijke99 <a.m.dusseljee@st.hanze.nl>
2018-11-01 12:42:12 +0100

    Frame voor instellingen toegevoegd

--- GUI.py
+++ GUI.py
@@ -1,66 +1,119 @@
 from tkinter import *
 from tkinter import ttk
+import tkinter as tk
 
 """In deze klasse wordt de layout geinitialiseerd. 
 Er word gebruik gemaakt van een canvas om de grafieken op te tekenen
 en er zijn tabbladen gemaakt voor de verschillende arduino's"""
-class App:
-    def __init__(self, master):
-        self.master = master
-        master.title('Centrale')
-        self.notebook = ttk.Notebook(master)
-        self.tab1 = ttk.Frame(master)
-        self.tab2 = ttk.Frame(master)
-        self.tab3 = ttk.Frame(master)
-        self.tab4 = ttk.Frame(master)
-        self.tab5 = ttk.Frame(master)
 
-        self.graph1 = Canvas(self.tab1, width=675, height=475)
-        self.graph2 = Canvas(self.tab2, width=675, height=475)
-        self.graph3 = Canvas(self.tab3, width=675, height=475)
-        self.graph4 = Canvas(self.tab4, width=675, height=475)
-        self.graph5 = Canvas(self.tab5, width=675, height=475)
+class App(tk.Tk):
 
-    def initialiseren(self):
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+        self.title('Centrale')
+        self.controller = self
+
+        container = tk.Frame(self)
+        container.pack(side="bottom", fill="both", expand=True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        self.frames = {}
+        self.frames["StartPage"] = StartPage(parent=container, controller=self)
+        self.frames["Instellingen"] = Instellingen(parent=container, controller=self)
+        self.frames["StartPage"].grid(row=0, column=0, sticky="nsew")
+        self.frames["Instellingen"].grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame("StartPage")
+        self.addtabs()
+
+    def show_frame(self, page_name):
+        '''Show a frame for the given page name'''
+        frame = self.frames[page_name]
+        frame.tkraise()
+
+    def addtabs(self):
+        self.notebook = ttk.Notebook(self.controller)
+        self.tab1 = ttk.Frame(self.controller)
+        self.tab2 = ttk.Frame(self.controller)
+        self.tab3 = ttk.Frame(self.controller)
+        self.tab4 = ttk.Frame(self.controller)
+        self.tab5 = ttk.Frame(self.controller)
         self.notebook.add(self.tab1, text='Arduino1')
         self.notebook.add(self.tab2, text='Arduino2')
         self.notebook.add(self.tab3, text='Arduino3')
         self.notebook.add(self.tab4, text='Arduino4')
         self.notebook.add(self.tab5, text='Arduino5')
-        self.notebook.grid(column=0, row=0)
+        self.notebook.pack()
 
-        self.graph1.create_rectangle([10, 10, 650, 450], fill='blue')   #placeholder voor grafiek
+        self.graph1 = Canvas(self.tab1, width=675, height=475)
+        self.graph1.create_rectangle([10, 10, 650, 450], fill='blue')  # placeholder voor grafiek
         self.graph1.grid(column=1, row=0)
-        self.graph2.create_rectangle([10, 10, 650, 450],fill='red')     #placeholder voor grafiek
+
+        self.graph2 = Canvas(self.tab2, width=675, height=475)
+        self.graph2.create_rectangle([10, 10, 650, 450], fill='red')  # placeholder voor grafiek
         self.graph2.grid(column=2, row=0)
+
+        self.graph3 = Canvas(self.tab3, width=675, height=475)
         self.graph3.grid(column=3, row=0)
+
+        self.graph4 = Canvas(self.tab4, width=675, height=475)
         self.graph4.grid(column=4, row=0)
+
+        self.graph5 = Canvas(self.tab5, width=675, height=475)
         self.graph5.grid(column=5, row=0)
 
-        self.uitrollen = Button(self.master, text="  Uitrollen   ", command=self.rolluik_uitrollen)
-        self.uitrollen.grid(column=0, row=1,sticky=W)
+class StartPage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
 
-        self.inhalen = Button(self.master, text="    Inhalen    ", command=self.rolluiken_inhalen)
-        self.inhalen.grid(column=0, row=1,sticky=W,padx=100)
+        button1 = tk.Button(self, text="Instellingen", command=lambda: instellingen())
+        button1.pack(side="right", padx=30)
 
-        self.stoppen = Button(self.master, text="    Stoppen    ", command=self.stoppen)
-        self.stoppen.grid(column=0, row=1,sticky=W,padx=200)
+        def instellingen():
+            app.notebook.destroy()
+            controller.show_frame("Instellingen")
 
-        self.instellingen = Button(self.master, text="   Instellingen    ", command=self.instellingen)
-        self.instellingen.grid(column=1,row=0,sticky=N,pady=25)
+        uitrollen = tk.Button(self, text="  Uitrollen   ", command=lambda: rolluik_uitrollen(self))
+        uitrollen.pack(side="left", padx=10)
 
-    def rolluik_uitrollen(self):
-        print("Aan het uitrollen")
+        inhalen = tk.Button(self, text="  Inhalen   ", command=lambda: rolluik_inhalen(self))
+        inhalen.pack(side="left", padx=10)
 
-    def rolluiken_inhalen(self):
-        print("Aan het inhalen")
+        stoppen = tk.Button(self, text="  Stoppen   ", command=lambda: stoppen(self))
+        stoppen.pack(side="left", padx=10)
 
-    def stoppen(self):
-        print("De rolluiken stoppen")
+        def rolluik_uitrollen(self):
+            print("Aan het uitrollen")
 
-    def instellingen(self):
-        print("Naar instelingen pagina gaan")
+        def rolluik_inhalen(self):
+            print("Aan het inhalen")
 
+        def stoppen(self):
+            print("De rolluiken stoppen")
 
+class Instellingen(tk.Frame):
 
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        label = tk.Label(self, text="Instellingen")
+        label.pack(side="top", fill="x", pady=10)
 
+        startpage = tk.Button(self, text="Terug", command=lambda: start())
+        startpage.pack(side="left", padx=10)
+
+        def start():
+            controller.show_frame("StartPage")
+            app.addtabs()
+
+        toepassen = tk.Button(self, text="Toepassen", command=lambda: toepassen(self))
+        toepassen.pack(side="right", padx=10)
+
+        def toepassen(self):
+            print("De instellingen zijn toegepast.")
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
