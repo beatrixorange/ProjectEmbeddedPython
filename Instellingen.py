670362deeb086eda1b71d9eb8da1c2f3ca5c8723
annemarijke99 <a.m.dusseljee@st.hanze.nl>
2018-11-07 12:15:14 +0100

    uitrol/inrolstand waardes naar int

--- Instellingen.py
+++ Instellingen.py
@@ -7,8 +7,8 @@ Hier kunnen de instellingen geregeld worden van de uitrol/inrolstand.
 
 ToDo:
 Hoe groot is het zonnescherm? (dus tot hoe ver kan het maximaal uitgerold worden?)
-Een aantal standen bedenken voor tot hoe ver je het scherm wil inrollen (standaard 0cm)  en uitrollen (standaard max)
-Dit moet doorgegeven worden om op het display te kunnen laten zien hoe ver het rolluik is uitgerold.
+De standen moeten worden doorgegeven worden om op het display te kunnen laten zien hoe ver het rolluik is uitgerold
+na inrollen/uirollen.
 """
 
 
@@ -20,10 +20,14 @@ class Instellingen(StartPage):
         label = tk.Label(self, text="Instellingen", font='Helvetica 18 bold')
         label.pack(side="top", fill="x", pady=10)
 
+        text = tk.Label(self, text="Op deze pagina kan je instellen wat de stand van het rolluik moet zijn na het "
+                                   "inrollen of uitrollen.\n _________________________")
+        text.pack(side="top", fill="x", pady=10)
+
         self.uitrol_label = tk.Label(self, text=" Maximale uitrolstand:")
         self.uitrol_label.pack()
         self.uitrol_choiceVar = tk.StringVar()
-        self.uitrol_choices = ("Stand 1", "Stand 2", "Stand 3", "Stand 4")
+        self.uitrol_choices = ("max", "-10 cm", "-20 cm", "-30 cm")
         self.uitrol_choiceVar.set(self.uitrol_choices[0])
         self.uitrol_cb = ttk.Combobox(self, textvariable=self.uitrol_choiceVar, values=self.uitrol_choices, width=15)
         self.uitrol_cb.pack(pady=10)
@@ -31,8 +35,8 @@ class Instellingen(StartPage):
         self.inrol_label = tk.Label(self, text="Maximale inrolstand:")
         self.inrol_label.pack()
         self.inrol_choiceVar = tk.StringVar()
-        self.inrol_choices = ("Stand 1", "Stand 2", "Stand 3", "Stand 4")
-        self.inrol_choiceVar.set(self.inrol_choices[3])
+        self.inrol_choices = ("min", "10 cm", "20 cm", "30 cm")
+        self.inrol_choiceVar.set(self.inrol_choices[0])
         self.inrol_cb = ttk.Combobox(self, textvariable=self.inrol_choiceVar, values=self.inrol_choices, width=15)
         self.inrol_cb.pack(pady=10)
 
@@ -42,6 +46,22 @@ class Instellingen(StartPage):
         terug = tk.Button(self, text="Terug", command=lambda: self.home(), width=10)
         terug.pack(fill="y", pady=10)
 
+        self.uitrolstand = 0
+        self.inrolstand = 0
+
     def toepassen(self):
-        print("De maximale uirolstand is nu:", self.uitrol_choiceVar.get())
-        print("De maximale inrolstand is nu:", self.inrol_choiceVar.get())
+        if self.uitrol_choiceVar.get() == self.uitrol_choices[0]:
+            self.uitrolstand = 0
+            print("De maximale uitrolstand is nu", self.uitrolstand, "cm.")
+        else:
+            self.uitrolstand = self.uitrol_choiceVar.get().split(' ')
+            self.uitrolstand = int(self.uitrolstand[0])
+            print("De maximale uitrolstand is nu:", self.uitrolstand, "cm.")
+
+        if self.inrol_choiceVar.get() == self.inrol_choices[0]:
+            self.inrolstand = 0
+            print("De maximale inrolstand is nu", self.inrolstand, "cm.")
+        else:
+            self.inrolstand = self.inrol_choiceVar.get().split(' ')
+            self.inrolstand = int(self.inrolstand[0])
+            print("De maximale inrolstand is nu:", self.inrolstand, "cm.")
