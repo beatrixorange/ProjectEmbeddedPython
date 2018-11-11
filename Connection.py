9a12bb54d4483de613a6a44c2243ba5e3eabaa55
danielgeerts7 <daniel.geerts@live.nl>
2018-11-11 15:54:32 +0100

    Extra check

--- Connection.py
+++ Connection.py
@@ -35,80 +35,81 @@ def inlezen(s):
     # TODO Uitvinden wat we met het ID doen.
     while True:
         x = s.read()
-        if(x != None and x != '' and x.hex != None and x.hex != ''):
-            x = bytes(x).decode()
-            if x == '#':
-                checker = False
-                id = 0
-                distance = []
-                count = 0
-                running = 1
-                while running == 1 and x != '&' and x != '$' and x != '%':
-                    x = s.read()
-                    if (ord(x) < 128):
-                        x = bytes(x).decode()
-                        if x != '.' and checker is False:       # fetch ID
-                            id = x
-                        elif x == '.' and checker is False:     # switch to number
-                            checker = True
-                        elif x != "." and x != '#' and checker is True:       # save number as distance
-                            distance.append(int(x))
-                            count = count + 1
-                        elif x == '#' and checker is True:
-                            running = 0
-                result = sum_array_elements (distance)
-                print("Distance: " + str(result) + ", ID: " + id)
-                Controller.frames["Arduino"+id].get_uitrol_afstand()['text'] = "Het rolluik is " + str(result) + " cm uitgerold";
+        if (ord(x) < 128):
+            if(x != None and x != '' and x.hex != None and x.hex != ''):
+                x = bytes(x).decode()
+                if x == '#':
+                    checker = False
+                    id = 0
+                    distance = []
+                    count = 0
+                    running = 1
+                    while running == 1 and x != '&' and x != '$' and x != '%':
+                        x = s.read()
+                        if (ord(x) < 128):
+                            x = bytes(x).decode()
+                            if x != '.' and checker is False:       # fetch ID
+                                id = x
+                            elif x == '.' and checker is False:     # switch to number
+                                checker = True
+                            elif x != "." and x != '#' and checker is True:       # save number as distance
+                                distance.append(int(x))
+                                count = count + 1
+                            elif x == '#' and checker is True:
+                                running = 0
+                    result = sum_array_elements (distance)
+                    print("Distance: " + str(result) + ", ID: " + id)
+                    Controller.frames["Arduino"+id].get_uitrol_afstand()['text'] = "Het rolluik is " + str(result) + " cm uitgerold";
 
-            if x == '$':
-                checker = 0
-                id = 0
-                brightness = []
-                running = 1
-                while running == 1  and x != '&' and x != '#' and x != '%':
-                    x = s.read()
-                    if (ord(x) < 128):
-                        x = bytes(x).decode()
-                        if x != '.' and checker == 0:  # fetch ID
-                            id = x
-                        elif x == '.' and checker == 0:  # switch to number
-                            checker = 1
-                        elif x != "." and x != '$' and checker == 1:  # save number as distance
-                            brightness.append(x)
-                        elif x == '$' and checker == 1:
-                            running = 0
-                result = sum_array_elements(brightness)
-                print("Light: " + str(result) + ", ID: " + id)
-                if (light_list.keys().__contains__(id)):
-                    light_list[id].append(int(result))
-                else:
-                    light_list[id] = []
-                    light_list[id].append(int(result))
+                if x == '$':
+                    checker = 0
+                    id = 0
+                    brightness = []
+                    running = 1
+                    while running == 1  and x != '&' and x != '#' and x != '%':
+                        x = s.read()
+                        if (ord(x) < 128):
+                            x = bytes(x).decode()
+                            if x != '.' and checker == 0:  # fetch ID
+                                id = x
+                            elif x == '.' and checker == 0:  # switch to number
+                                checker = 1
+                            elif x != "." and x != '$' and checker == 1:  # save number as distance
+                                brightness.append(x)
+                            elif x == '$' and checker == 1:
+                                running = 0
+                    result = sum_array_elements(brightness)
+                    print("Light: " + str(result) + ", ID: " + id)
+                    if (light_list.keys().__contains__(id)):
+                        light_list[id].append(int(result))
+                    else:
+                        light_list[id] = []
+                        light_list[id].append(int(result))
 
-            if x == '%':
-                checker = False
-                id = 0
-                temperatures = []
-                running = 1
-                while running == 1 and x != '&' and x != '$' and x != '#':
-                    x = s.read()
-                    if (ord(x) < 128):
-                        x = bytes(x).decode()
-                        if x != '.' and checker is False:  # fetch ID
-                            id = x
-                        elif x == '.' and checker is False:  # switch to number
-                            checker = True
-                        elif x != "." and x != '%' and checker is True:  # save number as distance
-                            temperatures.append(x)
-                        elif x == '%' and checker is True:
-                            running = 0
-                result = sum_array_elements(temperatures)
-                print("Temperature: " + str(result) + ", ID: " + id)
-                if (temperature_list.keys().__contains__(id)):
-                    temperature_list[id].append(int(result))
-                else:
-                    temperature_list[id] = []
-                    temperature_list[id].append(int(result))
+                if x == '%':
+                    checker = False
+                    id = 0
+                    temperatures = []
+                    running = 1
+                    while running == 1 and x != '&' and x != '$' and x != '#':
+                        x = s.read()
+                        if (ord(x) < 128):
+                            x = bytes(x).decode()
+                            if x != '.' and checker is False:  # fetch ID
+                                id = x
+                            elif x == '.' and checker is False:  # switch to number
+                                checker = True
+                            elif x != "." and x != '%' and checker is True:  # save number as distance
+                                temperatures.append(x)
+                            elif x == '%' and checker is True:
+                                running = 0
+                    result = sum_array_elements(temperatures)
+                    print("Temperature: " + str(result) + ", ID: " + id)
+                    if (temperature_list.keys().__contains__(id)):
+                        temperature_list[id].append(int(result))
+                    else:
+                        temperature_list[id] = []
+                        temperature_list[id].append(int(result))
 
 def stuur_id():
     i = 1
