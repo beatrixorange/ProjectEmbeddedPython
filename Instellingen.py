80fb651c112d8a7484ac8060e786aeac8bb2f8bb
annemarijke99 <a.m.dusseljee@st.hanze.nl>
2018-11-11 16:49:32 +0100

    Error wanneer input geen getal is

--- Instellingen.py
+++ Instellingen.py
@@ -22,53 +22,61 @@ class Instellingen(StartPage):
                                    "inrollen of uitrollen.\n _________________________")
         text.pack(side="top", fill="x", pady=10)
 
-        self.uitrol_label = tk.Label(self, text=" Maximale uitrolstand:")
+        self.uitrol_label = tk.Label(self, text="Inrolstand in cm:")
         self.uitrol_label.pack()
-        self.max_uitrolstand = tk.Entry(self)
-        self.max_uitrolstand.pack(pady=10)
+        self.inrolstand = tk.Entry(self)
+        self.inrolstand.pack(pady=10)
 
-        self.inrol_label = tk.Label(self, text="Maximale inrolstand:")
+        self.inrol_label = tk.Label(self, text="Uitrolstand in cm:")
         self.inrol_label.pack()
-        self.min_uitrolstand = tk.Entry(self)
-        self.min_uitrolstand.pack(pady=10)
+        self.uitrolstand = tk.Entry(self)
+        self.uitrolstand.pack(pady=10)
 
-        toepassen1 = tk.Button(self, text="Toepassen min", command=lambda: self.toepassen_min(), width=20)
+        toepassen1 = tk.Button(self, text="Toepassen inrolstand", command=lambda: self.toepassen_in(), width=20)
         toepassen1.pack(fill="y", pady=10)
 
-        toepassen2 = tk.Button(self, text="Toepassen max", command=lambda: self.toepassen_min(), width=20)
+        toepassen2 = tk.Button(self, text="Toepassen uitrolstand", command=lambda: self.toepassen_uit(), width=20)
         toepassen2.pack(fill="y", pady=5)
 
         terug = tk.Button(self, text="Terug", command=lambda: self.home(), width=10)
         terug.pack(fill="y", pady=10)
 
-    def toepassen_min(self):
-        min = self.min_uitrolstand.get()
-        i = 0
-        print(min+ " 1")
-        if min != None:
-            print(min+ " 2")
-            print(min+ " 3")
-            a = str(min)
-            char1 = "#".encode()
-            for c in Connection.connections:
-                c.write(char1)
-                while len(a) > i:
-                    numbers = a[i].encode()
-                    c.write(numbers)
-                    i = i + 1
-                c.write(char1)
+        self.error = tk.Label(self, text="Error: input moet een getal zijn", fg="red")
 
-    def toepassen_max(self):
-        max = self.min_uitrolstand.get()
-        i = 0
-        if min != None:
-            a = str(max)
-            char1 = "#".encode()
-            for c in Connection.connections:
-                c.write(char1)
-                while len(a) > i:
-                    numbers = a[i].encode()
-                    c.write(numbers)
-                    i = i + 1
-                c.write(char1)
+    def toepassen_in(self):
+        self.error.pack_forget()
+        if self.inrolstand.get().isdigit():
+            min = self.inrolstand.get()
+            i = 0
+            if min != None:
+                a = str(min)
+                char1 = "#".encode()
+                for c in Connection.connections:
+                    c.write(char1)
+                    while len(a) > i:
+                        numbers = a[i].encode()
+                        c.write(numbers)
+                        i = i + 1
+                    c.write(char1)
+        else:
+            self.error.pack(side="top", pady=10)
+
+
+    def toepassen_uit(self):
+        self.error.pack_forget()
+        if self.uitrolstand.get().isdigit():
+            max = self.uitrolstand.get()
+            i = 0
+            if min != None:
+                a = str(max)
+                char1 = "#".encode()
+                for c in Connection.connections:
+                    c.write(char1)
+                    while len(a) > i:
+                        numbers = a[i].encode()
+                        c.write(numbers)
+                        i = i + 1
+                    c.write(char1)
+        else:
+            self.error.pack(side="top", pady=10)
 
